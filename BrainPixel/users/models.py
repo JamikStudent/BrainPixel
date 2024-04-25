461883f102446aeacaadbaa61f5ce5c488195a1c
vsevolodmikheev <milanovf7@mail.ru>
2024-04-26 01:25:26 +0300

    test

--- BrainPixel/users/models.py
+++ BrainPixel/users/models.py
@@ -9,24 +9,24 @@ class User(models.Model):
     tips_first_type = models.IntegerField(default=0)  # Количество подсказок первого типа у пользователя
     tips_second_type = models.IntegerField(default=0)  # Количество подсказок второго типа у пользователя
 
-    def __str__(self):
-        """
-        Возвращает строковое представление объекта пользователя
-        """
-        return self.username
+    #def __str__(self):
+    #    """
+    #    Возвращает строковое представление объекта пользователя
+    #    """
+    #    return self.username
 
     def SubstractClueFirstType(self):
         """
-        Вычитает одну подсказку первого типа из общего количества у пользователя.
+        Вычитает одну подсказку первого типа из общего количества у пользователя
         """
-        self.tips_first_type -= 1  # Уменьшаем количество доступных подсказок первого типа на 1
+        self.tips_first_type -= 1
         self.save()  # Сохраняем изменения
     
     def SubstractClueSecondType(self):
         """
-        Вычитает одну подсказку второго типа из общего количества у пользователя.
+        Вычитает одну подсказку второго типа из общего количества у пользователя
         """
-        self.tips_second_type -= 1  # Уменьшаем количество доступных подсказок второго типа на 1
+        self.tips_second_type -= 1
         self.save()  # Сохраняем изменения
 
 class Topic(models.Model):
@@ -37,7 +37,7 @@ class Question(models.Model):
     text = models.TextField()
 
 class Answer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Using default user ID 1 as a placeholder
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
